2075cbd2d9a356953b4a6e01e298c2c3001e68fe
Vrishali-Mittal <vrishali.mittal@gmail.com>
2017-05-15 19:38:43 +0530

    Update Task5.cs

--- Task5.cs
+++ Task5.cs
@@ -6,59 +6,58 @@ using System.Threading.Tasks;
 
 namespace MachineTest_net_Vrishali
 {
- public class AddNumbers
- {
-  int result = 0, flag = 0;
-  AddNumbers()
-  {
-   result = 0;
-  }
-  public void addition(string[] arrValues)
-  {
-   try
-   {
-    for (int i = 0; i < arrValues.Length; i++)
-    {
-     if (arrValues[i] == "")
-     {
-      continue;
-     }
-     else if (int.Parse(arrValues[i]) < 0)
-     {
-      flag = 1;
-     }
-     else
-     {
-      result = result + int.Parse(arrValues[i]);
-     }
-    }
-   }
-   catch (FormatException e)
-   {
-    Console.WriteLine("Exception caught: {0}", e);
-   }
-   finally
-   {
-    if (flag == 1)
-    {
-     Console.WriteLine("Negative numbers not allowed");
-    }
-    else
-    {
-     Console.WriteLine("Sum of digits is:" + result);
-    }
-    Console.ReadLine();
-   }
-  }
-  public static void Main()
-  {
-   Console.WriteLine("Enter a Number:");
-   string num = Console.ReadLine();
-   string numbers = num.Replace(@"\\", @";"); //replace '//' with ';'
-   string[] values = numbers.Split(','); //putting values in array
-   AddNumbers add = new AddNumbers();
-   add.addition(values);
-  }
- }
+public class AddNumbers
+{
+int result = 0, flag = 0;
+AddNumbers()
+{
+result = 0;
 }
-
+public void addition(string[] arrValues)
+{
+try
+{
+for (int i = 0; i < arrValues.Length; i++)
+{
+if (arrValues[i] == "")
+{
+continue;
+}
+else if (int.Parse(arrValues[i]) < 0)
+{
+flag = 1;
+}
+else
+{
+result = result + int.Parse(arrValues[i]);
+}
+}
+}
+catch (FormatException e)
+{
+Console.WriteLine("Exception caught: {0}", e);
+}
+finally
+{
+if (flag == 1)
+{
+Console.WriteLine("Negative numbers not allowed");
+}
+else
+{
+Console.WriteLine("Sum of digits is:" + result);
+}
+Console.ReadLine();
+}
+}
+public static void Main()
+{
+Console.WriteLine("Enter a Number:");
+string num = Console.ReadLine();
+string numbers = num.Replace(@"\\", @";"); //replace '//' with ';'
+string[] values = numbers.Split(','); //putting values in array
+AddNumbers add = new AddNumbers();
+add.addition(values);
+}
+}
+}
