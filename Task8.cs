a0fc43ed0acae206ceb65dfe09ab48437ccca986
Vrishali-Mittal <vrishali.mittal@gmail.com>
2017-05-15 19:46:26 +0530

    Update Task8.cs

--- Task8.cs
+++ Task8.cs
@@ -6,20 +6,41 @@ using System.Threading.Tasks;
 
 namespace MachineTest_net_Vrishali
 {
-class Task8
+public class MultiplyNumbers
 {
-static void Main(string[] args)
+int result = 0;
+MulNumbers()
 {
-int mul = 1;
+result = 1;
+}
+public void multiplication(string[] arrValues)
+{
+try
+{
+for (int i = 0; i < arrValues.Length; i++)
+{
+{
+result = result * int.Parse(arrValues[i]);
+}
+}
+ 
+}
+catch (FormatException e)
+{
+Console.WriteLine("Exception caught: {0}", e);
+}
+finally
+{
+Console.WriteLine("Multiplication of digits of number is: {0}", result);
+}
+}
+public static void Main()
+{
 Console.WriteLine("Enter a Number:");
 string num = Console.ReadLine();
-string[] values = num.Split(',');
-for (int i = 0; i < values.Length; i++)
-{
-mul = mul * int.Parse(values[i]);            
-}
-Console.WriteLine("multiply of digits of number is:" + mul);
-Console.ReadLine();
+string[] values = num.Split(','); //putting values in array
+MulNumbers mul = new MulNumbers();
+mul.multiplication(values);
 }
 }
 }
